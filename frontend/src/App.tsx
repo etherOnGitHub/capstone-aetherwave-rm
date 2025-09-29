fd629cf42a225bf09beede584ae5b23bfa3100b4
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2025-09-29 21:18:26 +0100

    update: page design tweaks in react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,11 @@ import { useState } from 'react'
 import "./index.css";
 import Navbar from "./components/navbar";
 
-function RtnMsg() {
-    return <h1 className="text-3xl font-bold text-blue-500">Tailwind Works!</h1>
-}
-
 function App() {
   const [count, setCount] = useState(0)
 
     return (
+
         < Navbar />
   )
 }
