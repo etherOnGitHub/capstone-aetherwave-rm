2ae813dd1353b1d4f32e08a0b899ba0d6c159776
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2025-10-06 13:01:22 +0100

    add: login page modal pt 1
    added login page modal pt 1 so it doesnt take off main page shell in focus with blur

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,11 +13,11 @@ function App() {
                 < Navbar />
             </div>
             <div>
-                < div className="min-h-[calc(100vh-4rem)] mt-16 w-screen bg-brandBlack outline outline-2 outline-white">
+                < div className="bg-brandBlack mt-16 min-h-[calc(100vh-4rem)] w-screen outline-2 outline-white">
                     <div className="w-full">
                         < SynthModule />
                     </div>
-                    <div className="grid-cols-1 bg-brandBlack outline outline-2 outline-white xl:grid-cols-2">
+                    <div className="bg-brandBlack grid-cols-1 outline-2 outline-white xl:grid-cols-2">
                     </div>
                 </div>
             </div>
