7bb5d8b7a60a25fbbd2520805aac96a93e4e9f17
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2025-10-07 18:02:26 +0100

    add: py urls/views to support model database filtering
    user can now get filtered results on load in a dropdown

--- audioenv/views.py
+++ audioenv/views.py
@@ -1,13 +1,25 @@
 from django.http import JsonResponse
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Preset
 from .serializers import PresetSerializer
 
 class PresetListCreateView(generics.ListCreateAPIView):
-    queryset = Preset.objects.all()
     serializer_class = PresetSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Only return presets that belong to the logged-in user
+        return Preset.objects.filter(user=self.request.user).order_by('-created_at')
+
+    def perform_create(self, serializer):
+        # Automatically set the preset's user to the logged-in user
+        serializer.save(user=self.request.user)
 
 
+@api_view(['GET'])
 def auth_status(request):
     return JsonResponse({
         "authenticated": request.user.is_authenticated,
